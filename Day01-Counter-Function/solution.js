d0e3181b83146a209fc9d4fe8d1024003d4492c3
yasir-hakimi <217441985+yasir-hakimi@users.noreply.github.com>
2025-07-09 09:27:20 +0530

    Update solution.js

--- Day01-Counter-Function/solution.js
+++ Day01-Counter-Function/solution.js
@@ -1,1 +1,25 @@
-fs
+/**
+ * @return {Function}
+ */
+var createHelloWorld = ( a = "Hello World" ) => {
+    
+    return function(...args) {
+       return a;
+    }
+};
+
+
+
+// or
+var c = a()=>{
+  return function(...args){
+    return a; 
+  }
+}
+/**
+
+
+
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
